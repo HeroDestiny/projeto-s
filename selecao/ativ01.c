9004fb6895df4c72c0762c2383dc94664fcc3542
Paulo Henrique Soares Lima e Silva <59665236+HeroDestiny@users.noreply.github.com>
2024-02-26 17:22:07 +0000

    ativ01

---
+++ selecao/ativ01.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+    int a = 0;
+
+    printf("Digite um numero: ");
+    scanf("%d", &a);
+
+    if (a > 0)
+    {
+        printf("Maior do que zero\n");
+    }
+    else if (a < 0)
+    {
+        printf("Menor do que zero\n");
+    }
+    else
+    {
+        printf("Zero\n");
+    }
+    return 0;
+}
